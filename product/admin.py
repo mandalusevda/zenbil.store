d7f5232327a5fd6116b22cfbe66895559742d281
mandalusevda <86109108+mandalusevda@users.noreply.github.com>
2021-09-11 18:51:56 +0430

    Add files via upload

---
+++ product/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from . import models
+# Register your models here.
+
+admin.site.register(models.product1)
+# admin.site.register(models.product2)
---
+++ sevdaa-zenbil.store/README.md
@@ -0,0 +1,1 @@
+sevdaa-zenbil.store
